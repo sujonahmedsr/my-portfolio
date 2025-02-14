3975779a1413529fea299f5682bcd65dcae0e913
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-14 23:19:02 +0600

    deatils page

--- client/src/app/blogs/[id]/page.tsx
+++ client/src/app/blogs/[id]/page.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 
+export const metadata: Metadata = {
+    title: "Blogs Deatils",
+    description: "Explore my latest blogs on web development, programming, and tech trends. Stay updated with insights, tutorials, and coding tips!",
+};
 
 const BlogDetails = async ({ params }: { params: any }) => {
     const { id } = await params
@@ -24,28 +29,26 @@ const BlogDetails = async ({ params }: { params: any }) => {
 
     return (
         <section className='bg-gray-100 py-10 dark:bg-gray-900 dark:text-white'>
-            <div className="max-w-3xl mx-auto px-4">
-                <div className="max-w-3xl mx-auto bg-white p-6 rounded-2xl shadow-lg dark:bg-gray-800">
-                    <Image
-                        src={blog.image}
-                        alt={blog.title}
-                        width={700}
-                        height={500}
-                        className="w-full h-[60vh] object-cover rounded-lg"
-                    />
-                    <h1 className="text-3xl font-semibold mt-4">{blog.title}</h1>
-                    <p className="text-gray-600 text-sm mb-4 dark:text-gray-300">{blog.description}</p>
-                    <div className="text-gray-800 dark:text-gray-200">
-                        <p>{blog.description}</p>
-                    </div>
+            <div className="max-w-3xl mx-auto bg-white p-6 rounded-2xl shadow-lg dark:bg-gray-800 border">
+                <Image
+                    src={blog.image}
+                    alt={blog.title}
+                    width={700}
+                    height={500}
+                    className="w-full h-[60vh] object-cover rounded-lg"
+                />
+                <h1 className="text-3xl font-semibold mt-4">{blog.title}</h1>
+                <p className="text-gray-600 text-sm mb-4 dark:text-gray-300">{blog.description}</p>
+                <div className="text-gray-800 dark:text-gray-200">
+                    <p>{blog.description}</p>
+                </div>
 
-                    <div className="mt-6 text-center">
-                        <Link href="/blogs">
-                            <p className="text-blue-600 hover:text-blue-700 font-medium dark:text-blue-400 dark:hover:text-blue-500">
-                                ← Back to Blog List
-                            </p>
-                        </Link>
-                    </div>
+                <div className="mt-6 text-center">
+                    <Link href="/blogs">
+                        <p className="text-blue-600 hover:text-blue-700 font-medium dark:text-blue-400 dark:hover:text-blue-500">
+                            ← Back to Blog List
+                        </p>
+                    </Link>
                 </div>
             </div>
         </section>
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Metadata } from 'next';
 import Image from 'next/image';
 import React from 'react';
 import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa';
+
+export const metadata: Metadata = {
+    title: "Project Deatils",
+    description: "Explore my latest blogs on web development, programming, and tech trends. Stay updated with insights, tutorials, and coding tips!",
+};
 
 const ProjectDetailsPage = async ({ params }: { params: any }) => {
     const { id } = await params
@@ -31,40 +37,37 @@ const ProjectDetailsPage = async ({ params }: { params: any }) => {
 
     return (
         <section className="bg-gray-100 py-10 dark:bg-gray-900 dark:text-white">
-            <div className="max-w-3xl mx-auto px-4">
-                <h2 className="text-4xl font-bold mb-6">{project.title}</h2>
-                <div className="max-w-3xl mx-auto bg-white p-6 rounded-2xl shadow-lg dark:bg-gray-800">
-                    <Image
-                        src={project.image}
-                        alt={project.title}
-                        width={700} height={500}
-                        className="w-full h-[60vh] object-cover rounded-lg mb-6"
-                    />
-                    <div className="flex items-center justify-between py-4">
-                        {/* Live Demo Link with Icon */}
-                        <a
-                            href={project?.liveLink}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex items-center text-blue-600 font-medium hover:underline dark:text-blue-400 dark:hover:text-blue-500"
-                        >
-                            <FaExternalLinkAlt className="h-5 w-5 mr-2" /> Live Demo
-                        </a>
-                        {/* GitHub Link with Icon */}
-                        <a
-                            href={project.githubLink}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="flex items-center text-blue-600 font-medium hover:underline dark:text-blue-400 dark:hover:text-blue-500"
-                        >
-                            <FaGithub className="h-5 w-5 mr-2" /> GitHub
-                        </a>
-                    </div>
-                    <p className="text-gray-600 text-lg mb-6 dark:text-gray-300">{project.description}</p>
-                    <h1><span className='text-lg font-semibold'>Technologies</span>: {
-                        project?.technologies?.map((item, index) => <span key={index}>{item}, </span>)
-                        }</h1>
+            <div className="max-w-3xl mx-auto bg-white p-6 rounded-2xl shadow-lg dark:bg-gray-800 border">
+                <Image
+                    src={project.image}
+                    alt={project.title}
+                    width={700} height={500}
+                    className="w-full h-[60vh] object-cover rounded-lg mb-6"
+                />
+                <div className="flex items-center justify-between py-4">
+                    {/* Live Demo Link with Icon */}
+                    <a
+                        href={project?.liveLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center text-blue-600 font-medium hover:underline dark:text-blue-400 dark:hover:text-blue-500"
+                    >
+                        <FaExternalLinkAlt className="h-5 w-5 mr-2" /> Live Demo
+                    </a>
+                    {/* GitHub Link with Icon */}
+                    <a
+                        href={project.githubLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center text-blue-600 font-medium hover:underline dark:text-blue-400 dark:hover:text-blue-500"
+                    >
+                        <FaGithub className="h-5 w-5 mr-2" /> GitHub
+                    </a>
                 </div>
+                <p className="text-gray-600 text-lg mb-6 dark:text-gray-300">{project.description}</p>
+                <h1><span className='text-lg font-semibold'>Technologies</span>: {
+                    project?.technologies?.map((item, index) => <span key={index}>{item}, </span>)
+                }</h1>
             </div>
         </section>
     );
