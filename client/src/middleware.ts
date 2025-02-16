1ca03243027daa4bd48c78084f21bd547aaef547
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-02-17 01:24:32 +0600

    dashboard all path protected

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,4 +1,4 @@
 export { default } from "next-auth/middleware"
 
 // just login and register dekhte parbe authenticate user chara 
-export const config = { matcher: ["/dashboard"] }
+export const config = { matcher: ["/dashboard/:path"] }
